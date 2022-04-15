b362d05aec2369895c65f91e7cceb745d8ff38df
ktheofilus <66745607+ktheofilus@users.noreply.github.com>
2022-04-15 19:31:27 +0700

    added paging v3

--- app/src/main/java/com/example/myapplication/viewmodel/StoryListViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/StoryListViewModel.kt
@@ -1,21 +1,13 @@
 package com.example.myapplication.viewmodel
 
-import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.myapplication.api.*
 import com.example.myapplication.data.StoryRepository
-import com.example.myapplication.di.AppModule
-import com.example.myapplication.di.AppModule.dataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,7 +17,5 @@ class StoryListViewModel  @Inject constructor(
 
     var storyItem: LiveData<PagingData<ListStoryItem>> =
         storyRepository.getStory().cachedIn(viewModelScope)
-
-
 }
 
